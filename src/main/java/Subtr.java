a8f23ca6eb0a8d28a1701b3edc04fa887ba1712e
pochter123 <pochter123@gmail.com>
2019-08-14 15:16:26 +0400

    q

--- src/main/java/Subtr.java
+++ src/main/java/Subtr.java
@@ -1,12 +1,10 @@
 class Subtr {
     private int a;
     public int getA(int i, int y) {
-        try {
+
             a = i - y;
 
-        }catch (ArithmeticException e) {
-            System.out.println(e);
-        }
+
         return a;
     }
 }
